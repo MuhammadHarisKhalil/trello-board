97ebcb6e1fad632af702d251a836371377ed4fc8
haris3474 <haris.khalil@manafatech.com>
2024-03-13 16:32:47 +0500

    add animation

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
  import type { Column } from "~~/types";
+ import draggable from "vuedraggable";
  import { nanoid } from "nanoid";
 
   const columns = ref<Column[]>([
@@ -19,15 +20,25 @@
   ]);
 </script>
 <template>
- <div class="flex gap-4 overflow-x-auto item-start">
-    <div v-for="column in columns" :key="column.id"
-    class="column bg-gray-200 p-5 rounded min-w-[250px]"
-    >
+ <div>
+   <draggable
+   v-model="columns"
+   group="columns"
+   item-key="id"
+   :animation="150"
+   handle=".drag-handle"
+   class="flex gap-4 overflow-x-auto items-start"
+   >
+   <template #item="{element: column} : {element: Column}">
+      <div class="column bg-gray-200 p-5 rounded min-w-[250px]">
      <header class="mb-4 text-bold">
+     <DragHandle />
         {{ column.title }}
      </header>
      <TrelloBoardTask v-for="task in column.task" :key="task.id" :task="task"/>  
      <button class="text-gray-500"> + Add  a card</button>
     </div>
+   </template>
+   </draggable>
  </div>
 </template>
